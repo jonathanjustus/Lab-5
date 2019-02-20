d727db14bc6c28088afdff4a40aed77e3e7a99c1
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-02-19 23:29:00 -0600

    Complete execute method

--- Calculator.java
+++ Calculator.java
@@ -106,7 +106,38 @@ public class Calculator {
         // Condition on the number of tokens (number of strings in user input separated by spaces)
         switch(tokens.length)
         {
+        case 0 : 
+        {
+        	throw new CalculatorException("Illegal Token Length");
+        }
         
+        
+        case 1 : 
+        {
+        	if (tokens[0] == "quit") 
+        	{
+        		return Integer.MIN_VALUE;
+        	}
+        	else 
+        	{
+        		throw new CalculatorException("Illegal Command");
+        	}
+        }
+        
+        case 2 : 
+        {
+        	return calculateTwoTokens(tokens);
+        }
+        	
+        case 3 : 
+        {
+        	return calculateThreeTokens(tokens);
+        }
+        	
+        default : 
+        {
+        	throw new CalculatorException("Illegal Token Length");
+        }
         }
     }
 
